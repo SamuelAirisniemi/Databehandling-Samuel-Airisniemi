9f9ff9ca6755030e6d928574c739b523d4f449cd
Samuel Airisniemi <Samuel.Airisniemi@iths.se>
2025-11-03 11:55:17 +0100

    fortsättning på övning

--- Week1/Missing data and aggregations exercises/Find missing values.py
+++ Week1/Missing data and aggregations exercises/Find missing values.py
@@ -3,4 +3,7 @@ from data_utils import plot_missing_values
 
 path = pd.read_csv("Week1/Missing data and aggregations exercises/student-mat-missing-data.csv")
 
-plot_missing_values(path)
+#plot_missing_values(path)
+
+missing_freetime = path[path["freetime"].isna()]
+print(missing_freetime)
